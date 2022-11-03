9b9e1f4e492027cab4b78c28bdbd3b701046ffd0
Febry Ardiansyah <febrymuhammad80@gmail.com>
2022-11-03 19:15:04 +0700

    added validator to check gojek version

--- android/app/src/main/kotlin/com/example/rajagoput/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/rajagoput/MainActivity.kt
@@ -1,6 +1,37 @@
 package com.rajagoput.app
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+import android.content.pm.PackageManager
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.rajagoput.app/gojek"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+        call, result ->
+        if (call.method == "version") {
+            val version = getAppVersion()
+            result.success(version)
+        } else {
+            result.notImplemented()
+        }
+    }
+  }
+
+    private fun getAppVersion(): String {
+        try {
+            val a = applicationContext.packageManager.getPackageInfo("com.gojek.app", 0)
+
+            return a.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            throw e
+        }
+    }
 }
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,11 +6,13 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FluttertoastWebPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
